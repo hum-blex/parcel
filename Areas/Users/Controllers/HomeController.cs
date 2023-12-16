d500b174ba6d2ad039f6d4de8a02add5791858d3
humblex <praneeykauri@gmail.com>
2023-12-16 13:57:49 +0545

    Add project files.

---
+++ Areas/Users/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Parcel1.Models;
+using System.Diagnostics;
+
+namespace parcel.Areas.Users.Controllers
+{
+	[Area("Users")]
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Areas/Users/Controllers/MailController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parcel1.DataAccess.Repository.IRepository;
+using Parcel1.Models;
+
+
+namespace parcel.Areas.Users.Controllers
+{
+	[Area("Users")]
+	public class MailController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public MailController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			IEnumerable<mail> objMailList = _unitOfWork.Mail.GetAll().ToList();
+			return View(objMailList);
+		}
+
+		//GET
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		//POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(mail obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.Mail.Add(obj);
+				_unitOfWork.Save();
+				TempData["success"] = "Mail sent";
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+
+		//public IActionResult Delete(int? id)
+		//{
+		//	if (id == null || id ==0 ) 
+		//	{
+		//		return NotFound();
+		//	}
+		//	mail? mailFromdb = _mailRepo.Get(u=>u.Id == id);
+		//	if (mailFromdb == null)
+		//	{
+		//		return NotFound();
+		//	}
+		//	return View(mailFromdb);
+		//}
+	}
+}
